173191bf270fd61ca396ad29104dd3e50611b2d6
fidelis182 <fedymueni18@gmail.com>
2025-04-29 22:58:54 +0300

    creating and styling my navigation bar together with the entire application

--- foodapp/src/App.jsx
+++ foodapp/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import Search from "./components/Search";
 import FoodList from "./components/FoodList";
+import Navbar from "./components/Navbar";
+import "./app.css";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
   return (
     <div className="App">
+      <Navbar />
       {/* passing our array as props */}
       <Search foodData={foodData} setFoodData={setFoodData} />
       <FoodList foodData={foodData} />
---
+++ foodapp/src/components/Navbar.jsx
@@ -0,0 +1,4 @@
+import styles from "./module/Navbar.module.css";
+export default function Navbar() {
+  return <div className={styles.nav}> 🍔Food Recipes</div>;
+}
