a0fc0a1750a59da6d33df7a8dd1c2dc2c5a2012b
fidelis182 <fedymueni18@gmail.com>
2025-04-29 22:44:25 +0300

    displaying images of the recipe on the website or other fetching them

--- foodapp/src/components/FoodItem.jsx
+++ foodapp/src/components/FoodItem.jsx
@@ -1,3 +1,9 @@
-export default function FoodItem() {
-  return <div></div>;
+export default function FoodItem({ food }) {
+  return (
+    <div>
+      <img src={food.image} />
+      <h1>{food.title}</h1>
+      <button>View Recipe</button>
+    </div>
+  );
 }
--- foodapp/src/components/FoodList.jsx
+++ foodapp/src/components/FoodList.jsx
@@ -1,9 +1,11 @@
+import FoodItem from "./FoodItem";
+
 export default function FoodList({ foodData }) {
   return (
     <div>
       {/* to display the data to our website */}
       {foodData.map((food) => (
-        <h1>{food.title}</h1>
+        <FoodItem key={food.id} food={food} />
       ))}
     </div>
   );
