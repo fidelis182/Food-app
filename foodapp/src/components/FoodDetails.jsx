16927d56c721b13497b769df99c20682348d2f53
fidelis182 <fedymueni18@gmail.com>
2025-05-12 23:18:50 +0300

    fetching the ingredients

--- foodapp/src/components/FoodDetails.jsx
+++ foodapp/src/components/FoodDetails.jsx
@@ -46,7 +46,22 @@ export default function FoodDetails({ foodId }) {
             $<strong> {food.pricePerServing / 100}</strong>
           </span>
         </div>
-
+        <h2>Ingredients</h2>
+        {food?.extendedIngredients?.map((items) => (
+          <div>
+            <img
+              src={
+                `https://spoonacular.com/cdn/ingredients_100x100/` + items.image
+              }
+              alt=""
+            />
+            <h3>{items.name}</h3>
+            <h3>
+              {items.amount}
+              {items.unit}
+            </h3>
+          </div>
+        ))}
         <h2>Instructions</h2>
         <div className={styles.recipeInstructions}>
           <ol>
