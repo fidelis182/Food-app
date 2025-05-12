ab6ab19f79b80fdaef33e4d092eafae9ecb57152
fidelis182 <fedymueni18@gmail.com>
2025-05-12 14:40:25 +0300

    fetching recipes from API using useEffect hook

--- foodapp/src/components/FoodDetails.jsx
+++ foodapp/src/components/FoodDetails.jsx
@@ -1,3 +1,24 @@
+import { useEffect, useState } from "react";
+
 export default function FoodDetails({ foodId }) {
-  return <div>Food Deatils {foodId}</div>;
+  const [food, setFood] = useState({});
+  const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_KEY = "85bba1f5472e43498d1d6c685f838035";
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?apiKey=${API_KEY}`);
+      const data = await res.json(); // ✅ Correct method
+      console.log(data);
+      setFood(data);
+    }
+    fetchFood();
+  }, [foodId]);
+
+  return (
+    <div>
+      Food Deatils {foodId}
+      {food.title}
+      <img src={food.image} alt="" />
+    </div>
+  );
 }
