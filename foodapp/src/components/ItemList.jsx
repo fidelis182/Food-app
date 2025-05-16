a55ebe0ef27842a6eb7a5c17860a91529c54b7b1
fidelis182 <fedymueni18@gmail.com>
2025-05-16 11:34:37 +0300

    spliting item component into different components

--- foodapp/src/components/ItemList.jsx
+++ foodapp/src/components/ItemList.jsx
@@ -1,24 +1,14 @@
+import Item from "./Item";
+
+// import styles from "./Module/item.module.css";
 export default function ItemList({ food, isLoading }) {
-  return;
-  <div>
-    {isLoading ? (
-      <p>Loading</p>
-    ) : (
-      food?.extendedIngredients?.map((items) => (
-        <div>
-          <img
-            src={
-              `https://spoonacular.com/cdn/ingredients_100x100/` + items.image
-            }
-            alt=""
-          />
-          <h3>{items.name}</h3>
-          <h3>
-            {items.amount}
-            {items.unit}
-          </h3>
-        </div>
-      ))
-    )}
-  </div>;
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading....</p>
+      ) : (
+        food.extendedIngredients?.map((item) => <Item item={item} />)
+      )}
+    </div>
+  );
 }
