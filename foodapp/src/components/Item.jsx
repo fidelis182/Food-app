202e487c5c4f16ddc9d2daf791598048ffe9ee91
fidelis182 <fedymueni18@gmail.com>
2025-05-16 11:52:44 +0300

    completed the food recipe app

--- foodapp/src/components/Item.jsx
+++ foodapp/src/components/Item.jsx
@@ -1,16 +1,22 @@
+import styles from "./Module/item.module.css";
 export default function Item({ item }) {
   return (
     <div>
-      <div>
-        <img
-          src={`https://spoonacular.com/cdn/ingredients_100x100/${item.image}`}
-          alt={item.name}
-        />
-        <h3>{item.name}</h3>
-        <h3>
-          {item.amount}
-          {item.unit}
-        </h3>
+      <div className={styles.itemContainer}>
+        <div className={styles.imageContainer}>
+          <img
+            className={styles.image}
+            src={`https://spoonacular.com/cdn/ingredients_100x100/${item.image}`}
+            alt={item.name}
+          />
+        </div>
+        <div className={styles.nameContainer}>
+          <div className={styles.name}>{item.name}</div>
+          <div className={styles.amount}>
+            {item.amount}
+            {item.unit}
+          </div>
+        </div>
       </div>
     </div>
   );
